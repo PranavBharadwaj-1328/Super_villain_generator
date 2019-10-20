046fe00358548090d274e468b62a754cdef7ab03
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2019-10-20 15:25:10 +0530

    Add files via upload

---
+++ supervillan.py
@@ -0,0 +1,9 @@
+def supvill():
+  import random
+  a = ['Mr.','Mrs.','Dark','Super','Evil','Space','Master','Long','Dean','Fiery','Jeetha','clumsy','Billy','Dead','Sonic']
+  b = [' Destructo',' Electro',' Magneto',' Batman',' Joker',' Diaper',' Nose picker',' Banana',' Spell check',' Shot',' Boom',' Farty']
+  c = [' Man',' Woman',' Lord',' Bandit',' Slaughterer',' Burner',' Geek',' Nerd',' Head']
+  p = random.choice(a)
+  q = random.choice(b)
+  r = random.choice(c)
+  print p+q+r
