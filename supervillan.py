6c01ea45a7f10ba5c822619f50b0174c3f7c5b29
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2019-11-02 11:56:45 +0530

    Update supervillan.py

--- supervillan.py
+++ supervillan.py
@@ -1,6 +1,6 @@
 def supvill():
   import random
-  a = ['Mr.','Mrs.','Dark','Super','Evil','Space','Master','Long','Dean','Fiery','Jeetha','clumsy','Billy','Dead','Sonic']
+  a = ['Mr.','Mrs.','Dark','Super','Evil','Space','Master','Long','Dean','Fiery','clumsy','Billy','Dead','Sonic']
   b = [' Destructo',' Electro',' Magneto',' Batman',' Joker',' Diaper',' Nose picker',' Banana',' Spell check',' Shot',' Boom',' Farty']
   c = [' Man',' Woman',' Lord',' Bandit',' Slaughterer',' Burner',' Geek',' Nerd',' Head']
   p = random.choice(a)
